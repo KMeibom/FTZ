37fabf0d8852f19758230e9fdfbb60ec1067a956
kasper meibom <Kaspermeibom@hotmail.com>
2019-12-11 23:03:06 +0100

    workin'

--- javascript.js
+++ javascript.js
@@ -4,13 +4,13 @@ function checkmember(memberlist, mail) {
 	memberlist[0] = "c.s.hoepner@gmail.com";
 	memberlist[1] = "kaspermeibom@hotmail.com";
 	memberlist[2] = "avibavi@line.com";
-	
-	var mail = document.getElementById("mail").value;
+	var mail =	document.getElementById("mail").value;
     
 	for (var i = 0; i <= memberlist.length; i++) {
-        if (memberlist[i] === mail) {
+        if (memberlist[i] == mail) {
            return alert(mail + " kører allerede smartere ;)");
         }
     }
+	memberlist.push(mail);
     return alert(mail + " doesn't exist");
 }
