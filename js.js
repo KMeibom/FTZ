6db295bf44755e1faa9122b353f0179adbea1608
kasper meibom <Kaspermeibom@hotmail.com>
2019-12-11 14:08:19 +0100

    skift bil funktion

--- js.js
+++ js.js
@@ -28,3 +28,21 @@ const navSlide=()=>
 }
 
 navSlide();
+
+var ImageIndex = 0;
+
+  function swipe(event, direction) {
+    var midpoint = Math.floor(screen.width / 2);
+    var px = event.pageX;
+    var items = document.getElementsByClassName('bil-model-wrapper');
+    var itemActive = items[ImageIndex];
+    if (direction === 'left') {
+      itemActive.style.marginLeft = '-100%';
+      itemActive.style.transition = '0.5s ';
+      ImageIndex = ImageIndex < items.length - 1 ? ImageIndex + 1 : ImageIndex;
+    } else {
+      itemActive.style.marginLeft = '0';
+      itemActive.style.transition = '0.5s ';
+      ImageIndex = ImageIndex >= 1 ? ImageIndex - 1 : 0;
+    }
+  }
