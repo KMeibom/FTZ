394a549739c7d3c7935cac055e1fc8af81380e54
kasper meibom <Kaspermeibom@hotmail.com>
2019-12-14 20:54:04 +0100

    farveskift på advarsel

--- js-warning-function.js
+++ js-warning-function.js
@@ -1,25 +1,16 @@
-
-function warning() {
-	var warning = document.getElementById("warning-section");
+var warning = document.getElementById("warning-section");
+function advarsel() {
   if (warning.style.display === "none") {
-    warning.style.display = "flex";
-  } else {
+    warning.style.display = "flex";  
+  }
+else {
     warning.style.display = "none";
   }
-	var seconds = 300;
-	
-	function changeColor() {
-		if (seconds <= 300 && seconds > 90) {
-			document.warning.style.background = "#4587E1";
-		}
-
-		else if (seconds <= 90 && seconds > 30) {
-			document.warning.style.background = "#A9D7BB";
-		}
-
-		else {
-			document.warning.style.background = "4587E1";
-		}
-	}
-	changeColor();
+var i = 0;
+function change() {
+  var color = ["#4587E1", "#A9D7BB"];
+  warning.style.backgroundColor = color[i];
+  i = (i + 1) % color.length;
+}
+setInterval(change, 1500);
 }
